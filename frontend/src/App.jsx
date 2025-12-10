b57eadcb148dc6b619ab79709f4f409fb6cea9ce
mark-wrc <markstorey.wrc@gmail.com>
2025-12-10 16:28:44 +0800

    installing react, tailwind, shadcn, and lucide icon in frontend folder

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+
+const App = () => {
+  return (
+    <div>
+      <Button variant="destructive" className="font-mono">Click me</Button>
+    </div>
+  );
+};
+
+export default App;
